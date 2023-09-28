b6c6d31d0d58207d60cc16602c84d3a09264d485
Oles Malysh <olesmalish37@gmail.com>
2023-09-28 16:41:15 +0200

    3 questions work correct

--- app/src/main/java/com/example/myquiz/Quiz.kt
+++ app/src/main/java/com/example/myquiz/Quiz.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompatSideChannelService
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.findNavController
@@ -19,12 +20,22 @@ data class QuizQuestions(
 
 class Quiz : Fragment() {
     lateinit var binding: FragmentQuizBinding
-    var currentAnswer = 0
+    var currentRightAnswer = 0
     var currentQuestionIndex = 0
     val COUNT_ANSWERS = 3
 
     var listOfQuestions = listOf<QuizQuestions>(
-        QuizQuestions("How many states are there in the USA?", listOf("49", "50", "51"), 1)
+        QuizQuestions("How many states are there in the USA?", listOf("49", "50", "51"), 1),
+        QuizQuestions(
+            "What country has the highest life expectancy?",
+            listOf("Hong Kong", "Japan", "China"),
+            0
+        ),
+        QuizQuestions(
+            "What game studio makes the Red Dead Redemption series?",
+            listOf("Microsoft", "GSC Game World", "Rockstar Games"),
+            2
+        )
 
     )
 
@@ -38,29 +49,42 @@ class Quiz : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentQuizBinding.inflate(inflater, container, false)
+
+        var idElem: Int = 0
+        randomizeQuestions()
         loadQuestion()
 
+
+
         binding.btnAnswerSend.setOnClickListener { view: View ->
-            if (currentAnswer < COUNT_ANSWERS) {
-                var checkedId = binding.rbGroup.checkedRadioButtonId
-                val currentQuestion = listOfQuestions[currentQuestionIndex]
-                var idElem: Int = 0
-                when (checkedId) {
-                    binding.rb0.id -> idElem = 0
-                    binding.rb1.id -> idElem = 1
-                    binding.rb2.id -> idElem = 2
-                }
-
-                if (currentQuestion.correctAnswer == idElem){
-                    view.findNavController().navigate(R.id.action_quiz_to_fragmentWon)
-                }
-                else view.findNavController().navigate(R.id.action_quiz_to_fragmentGameOver)
-
-
-
+            var currentQuestion = listOfQuestions[currentQuestionIndex]
+            var checkedId = binding.rbGroup.checkedRadioButtonId
+            var idElem: Int = 0
+            when (checkedId) {
+                binding.rb0.id -> idElem = 0
+                binding.rb1.id -> idElem = 1
+                binding.rb2.id -> idElem = 2
             }
 
+            if (currentQuestion.correctAnswer == idElem) {
+                currentRightAnswer++
+            }
+            else {
+                view.findNavController().navigate(R.id.action_quiz_to_fragmentGameOver)
+                return@setOnClickListener //What if delete?
+            }
+            currentQuestionIndex++
 
+            if (currentRightAnswer < COUNT_ANSWERS){
+                loadQuestion()
+            }
+
+            else {
+                view.findNavController().navigate(R.id.action_quiz_to_fragmentWon)
+                currentRightAnswer = 0
+                currentQuestionIndex = 0
+                randomizeQuestions()
+            }
         }
 
         return binding.root
